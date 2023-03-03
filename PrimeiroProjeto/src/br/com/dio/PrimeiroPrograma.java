2b92f0adbe9d26075b3ce52bc6a65fb467e6179f
JoaoGuiMachado <joao.gmachado14@gmail.com>
2023-03-03 14:54:15 -0300

    commita

---
+++ PrimeiroProjeto/src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,14 @@
+package br.com.dio;
+
+public class PrimeiroPrograma {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Gato gato = new Gato();
+		
+		System.out.println(gato);
+		
+	}
+
+}
